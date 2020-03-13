b873f0e5617c6d02fb859feb642eef1e443b029c
Axel Espinosa <axeldavid45@gmail.com>
2020-03-13 11:02:37 -0600

    Agregamos comentarios y un meta para crear .jar

--- actividad1/Activity1.java
+++ actividad1/Activity1.java
@@ -15,18 +15,19 @@ public class Activity1 {
 class Window extends JFrame {
     public Window () {
         this.setTitle("Activity 1 ");
+
         this.setSize(340, 250);
+
+        //Set borderLayout for JFrame
         this.setLayout(new BorderLayout());
 
         Layout panel = new Layout();
-
         this.add(panel, BorderLayout.CENTER);
 
-
         this.setVisible(true);
-
+        //Not resizable
         this.setResizable(false);
-
+        //Close
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
@@ -34,8 +35,10 @@ class Window extends JFrame {
 
 
 class Layout extends JPanel {
+    //Components
     JButton button = new JButton("I'm a button");
     JLabel label = new JLabel("Times you've clicked the button: 0");
+    //Click watcher
     int clickTimes;
 
     public Layout() {
